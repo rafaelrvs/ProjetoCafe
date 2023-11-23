d576c9f4f3b9f286573c6b5fcf3cc47dd5d7ed6f
rafaelrvs <rafaelvieira1333@gmail.com>
2023-11-23 20:16:45 -0300

    adicionando btn

--- app/src/main/java/com/rafael/mycafe/MainActivity.kt
+++ app/src/main/java/com/rafael/mycafe/MainActivity.kt
@@ -2,10 +2,22 @@ package com.rafael.mycafe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var btnLogin:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            println("text")
+        }
+
     }
 }
